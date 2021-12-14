1d93db30a74aa1b7224894f91f9d71e121d99f15
DevExpressExampleBot <fakeEmail>
2021-12-14 17:07:53 +0300

    Source auto update [skip ci]

--- VB/XpoNorthwindServerMode/Window1.xaml.vb
+++ VB/XpoNorthwindServerMode/Window1.xaml.vb
@@ -1,54 +1,44 @@
-﻿Imports Microsoft.VisualBasic
-Imports System
-Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
 Imports System.Windows
 Imports System.Windows.Controls
-Imports System.Windows.Data
-Imports System.Windows.Documents
-Imports System.Windows.Input
-Imports System.Windows.Media
-Imports System.Windows.Media.Imaging
-Imports System.Windows.Navigation
-Imports System.Windows.Shapes
 Imports DevExpress.Xpo
 Imports DevExpress.Xpo.DB
 
 Namespace XpoNorthwindServerMode
-	''' <summary>
-	''' Interaction logic for Window1.xaml
-	''' </summary>
-	Partial Public Class Window1
-		Inherits Window
-		Public Sub New()
-			Dim conn As String = MSSqlConnectionProvider.GetConnectionString("(local)", "NorthwindXpo")
-			XpoDefault.DataLayer = XpoDefault.GetDataLayer(conn, AutoCreateOption.SchemaAlreadyExists)
 
-			InitializeComponent()
+    ''' <summary>
+    ''' Interaction logic for Window1.xaml
+    ''' </summary>
+    Public Partial Class Window1
+        Inherits Window
 
-			System.Diagnostics.Trace.Listeners.Add(New MyTraceListner(textBox1))
+        Public Sub New()
+            Dim conn As String = MSSqlConnectionProvider.GetConnectionString("(local)", "NorthwindXpo")
+            XpoDefault.DataLayer = XpoDefault.GetDataLayer(conn, AutoCreateOption.SchemaAlreadyExists)
+            Me.InitializeComponent()
+            System.Diagnostics.Trace.Listeners.Add(New MyTraceListner(Me.textBox1))
+            Dim uow As UnitOfWork = New UnitOfWork()
+            Dim col As XPServerCollectionSource = New XPServerCollectionSource(uow, GetType(NorthwindXpo.Order))
+            col.DisplayableProperties = "Customer.CompanyName;Employee.LastName;OrderDate;ShippedDate;" & "Freight;ShipName;ShipAddress;ShipCity;ShipRegion;ShipPostalCode;ShipCountry"
+            Me.gridControl1.ItemsSource = col
+            Me.gridControl1.PopulateColumns()
+        End Sub
+    End Class
 
-			Dim uow As New UnitOfWork()
-			Dim col As New XPServerCollectionSource(uow, GetType(NorthwindXpo.Order))
-			col.DisplayableProperties = "Customer.CompanyName;Employee.LastName;OrderDate;ShippedDate;" & "Freight;ShipName;ShipAddress;ShipCity;ShipRegion;ShipPostalCode;ShipCountry"
+    Friend Class MyTraceListner
+        Inherits System.Diagnostics.TraceListener
 
-			gridControl1.ItemsSource = col
-			gridControl1.PopulateColumns()
-		End Sub
-	End Class
+        Private outputWindow As TextBox
 
-	Friend Class MyTraceListner
-		Inherits System.Diagnostics.TraceListener
-		Private outputWindow As TextBox
-		Public Sub New(ByVal outputWindow As TextBox)
-			Me.outputWindow = outputWindow
-		End Sub
-		Public Overrides Overloads Sub Write(ByVal message As String)
-			outputWindow.AppendText(message)
-		End Sub
-		Public Overrides Overloads Sub WriteLine(ByVal message As String)
-			outputWindow.AppendText(message & Constants.vbCrLf)
-		End Sub
-	End Class
+        Public Sub New(ByVal outputWindow As TextBox)
+            Me.outputWindow = outputWindow
+        End Sub
+
+        Public Overrides Sub Write(ByVal message As String)
+            outputWindow.AppendText(message)
+        End Sub
+
+        Public Overrides Sub WriteLine(ByVal message As String)
+            outputWindow.AppendText(message & Microsoft.VisualBasic.Constants.vbCrLf)
+        End Sub
+    End Class
 End Namespace
